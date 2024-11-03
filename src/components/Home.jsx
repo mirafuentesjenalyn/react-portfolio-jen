423dca320b8712815d7a310392ae0fc75c4c26d6
mirafuentesjenalyn <mirafuentesjenalyn@gmail.com>
2024-11-03 18:27:46 +0800

    change resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -123,7 +123,7 @@ const Home = () => {
                             whileTap={{ scale: 0.8, transition: { type: "spring", stiffness: 400, damping: 10 } }}
                             className="py-3 px-8 text-sm sm:text-base font-bold w-3/4 sm:w-auto bg-[#0a192f] text-[#64ffda] hover:bg-[#1BBBFF] hover:text-[#0a192f] border border-[#64ffda] rounded-full" // Updated rounded class
                             // className="py-3 px-8 text-sm sm:text-base font-bold w-full sm:w-auto bg-[#1BBBFF] text-[#0a192f] hover:bg-[#0a192f] hover:text-[#64ffda] border border-[#64ffda] rounded"
-                            onClick={() => window.open('https://drive.google.com/drive/u/1/folders/1UcE7YwUU9tajxKkZafSvhXS5zXGk-vmf', '_blank')}
+                            onClick={() => window.open('https://drive.google.com/file/d/1PPFjyZvQQVONJ8WNBG9CWmrqzKXsuSQP/view?usp=sharing', '_blank')}
                         >
                             Resume
                         </motion.button>
